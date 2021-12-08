7d4b94f191d15ca072f1a70cebaf72c1bedae2be
Hafiz Muhammad Rauf <hafiz.rauf@tkxel.io>
2021-12-08 05:04:12 +0500

    MVVM Cross Network Plugin Initial Commit

---
+++ MVVMCross.Plugin.Network/Rest/MvxPluginLog.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MS-PL license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Extensions.Logging;
+using MvvmCross.Logging;
+
+namespace MvvmCross.Plugin.Network
+{
+    internal static class MvxPluginLog
+    {
+        internal static ILogger Instance { get; } = MvxLogHost.GetLog("MvxPlugin.FieldBinding");
+    }
+}
